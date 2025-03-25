734670d9ed76b9699daa815f42f911cd42fd1ca5
Brandon Underwood <147886188+TheBeeseChurger@users.noreply.github.com>
2025-03-25 16:50:48 -0500

    Added Credits Scene

--- ProjectPluto/Assets/Scripts/ProjectManager.cs
+++ ProjectPluto/Assets/Scripts/ProjectManager.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ProjectManager : MonoBehaviour
 {
-    
+    [SerializeReference] private Scene credits_scene;
+
+    private async void Start()
+    {
+        await SceneManager.LoadSceneAsync(credits_scene.name, LoadSceneMode.Additive);
+    }
 }
