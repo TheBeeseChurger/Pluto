6c6f1287540793c3ef80bbc70c298e712d21ee41
Brandon Underwood <147886188+TheBeeseChurger@users.noreply.github.com>
2025-03-25 21:24:36 -0500

    Added Credits Sequence

--- ProjectPluto/Assets/Scripts/OpeningScript.cs
+++ ProjectPluto/Assets/Scripts/OpeningScript.cs
@@ -1,16 +1,75 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class OpeningScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private List<Credit> credits;
+
+    [SerializeField] private float start_up_time;
+    [SerializeField] private float time_between_credits;
+
+    public async void RunCredits()
     {
-        
+        await Awaitable.WaitForSecondsAsync(start_up_time);
+
+        foreach (var credit in credits)
+        {
+            credit.head.SetActive(true);
+
+            await FadeIn(credit, credit.fade_in_time);
+            await Awaitable.WaitForSecondsAsync(credit.credit_time);
+            await FadeOut(credit, credit.fade_out_time);
+
+            credit.head.SetActive(false);
+
+            await Awaitable.WaitForSecondsAsync(time_between_credits);
+        }
+
+        Debug.Log("Finished Credits");
     }
 
-    // Update is called once per frame
-    void Update()
+    private async Awaitable FadeIn(Credit credit, float time)
     {
+        float curr_time = 0f;
         
+        while (curr_time < time)
+        {
+            float alpha = Mathf.Lerp(credit.cover_reference.color.a, 0f, curr_time / time);
+
+            credit.cover_reference.color = new Color(credit.cover_reference.color.r, credit.cover_reference.color.g, credit.cover_reference.color.b, alpha);
+
+            await Awaitable.NextFrameAsync();
+            curr_time += Time.deltaTime;
+        }
+
+        credit.cover_reference.color = new Color(credit.cover_reference.color.r, credit.cover_reference.color.g, credit.cover_reference.color.b, 0f);
     }
+
+    private async Awaitable FadeOut(Credit credit, float time)
+    {
+        float curr_time = 0f;
+
+        while (curr_time < time)
+        {
+            float alpha = Mathf.Lerp(credit.cover_reference.color.a, 1f, curr_time / time);
+
+            credit.cover_reference.color = new Color(credit.cover_reference.color.r, credit.cover_reference.color.g, credit.cover_reference.color.b, alpha);
+
+            await Awaitable.NextFrameAsync();
+            curr_time += Time.deltaTime;
+        }
+
+        credit.cover_reference.color = new Color(credit.cover_reference.color.r, credit.cover_reference.color.g, credit.cover_reference.color.b, 1f);
+    }
+}
+
+[Serializable]
+public class Credit
+{
+    public GameObject head;
+    public SpriteRenderer cover_reference;
+    public float credit_time;
+    public float fade_in_time;
+    public float fade_out_time;
 }
--- ProjectPluto/Assets/Scripts/ProjectManager.cs
+++ ProjectPluto/Assets/Scripts/ProjectManager.cs
@@ -3,10 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class ProjectManager : MonoBehaviour
 {
-    [SerializeReference] private Scene credits_scene;
+    private const int SCENE_LOADING = 1;
+    private const int SCENE_CREDITS = 2;
+    private const int SCENE_MENU = 3;
+    private const int SCENE_GAMEIDLE = 4;
+    private const int SCENE_INTERMEDIARY = 5;
+    private const int SCENE_GAME = 6;
 
     private async void Start()
     {
-        await SceneManager.LoadSceneAsync(credits_scene.name, LoadSceneMode.Additive);
+        await SceneManager.LoadSceneAsync(SCENE_CREDITS, LoadSceneMode.Additive);
     }
 }
